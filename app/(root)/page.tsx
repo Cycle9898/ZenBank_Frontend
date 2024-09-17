ee96460bf8855db15faff1b31a05b91bfa2fceab
Cycle9898 <cycle9898@gmail.com>
2024-09-17 18:07:15 +0200

    integrate homepage header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 function Home() {
-	return <div>Home</div>;
+	const loggedIn = { firstName: "testUser" };
+
+	return (
+		<section className="home">
+			<div className="home-content">
+				<header className="home-header">
+					<HeaderBox
+						type="greeting"
+						title="Bienvenue"
+						user={loggedIn.firstName || "Guest"}
+						subtext="Accédez et gérez votre compte ou vos transactions efficacement."
+					/>
+
+					<TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.35} />
+				</header>
+			</div>
+		</section>
+	);
 }
 
 export default Home;
---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import CountUp from "react-countup";
+
+function AnimatedCounter({ amount }: { amount: number }) {
+	return (
+		<div className="w-full">
+			<CountUp decimal="," decimals={2} prefix="€" end={amount} />
+		</div>
+	);
+}
+
+export default AnimatedCounter;
