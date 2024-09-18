f3584bed44424fd21fd7b89595772c65501cafc9
Cycle9898 <cycle9898@gmail.com>
2024-09-18 16:00:50 +0200

    add some translations on homepage

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -9,12 +9,12 @@ function DoughnutChart({ accounts }: DoughnutChartProps) {
 	const data = {
 		datasets: [
 			{
-				label: "Banks",
+				label: "Banques",
 				data: [1250, 2587, 4563],
 				backgroundColor: ["#0747B6", "#2265D8", "#2F91FA"]
 			}
 		],
-		labels: ["Bank 1", "Bank 2", "Bank 3"]
+		labels: ["Banque 1", "Banque 2", "Banque 3"]
 	};
 
 	return (
--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -10,11 +10,11 @@ function TotalBalanceBox({ accounts = [], totalBanks, totalCurrentBalance }: Tot
 
 			<div className="flex flex-col gap-6">
 				<h2 className="header-2">
-					{totalBanks} Bank {totalBanks > 1 ? "Accounts" : "Account"}
+					{totalBanks}&nbsp;{totalBanks > 1 ? "Comptes Bancaires" : "Compte Bancaire"}
 				</h2>
 
 				<div className="flex flex-col gap-2">
-					<p className="total-balance-label">Total Current Balance</p>
+					<p className="total-balance-label">Solde Total Actuel</p>
 
 					<div className="total-balance-amount flex-center gap-2">
 						<AnimatedCounter amount={totalCurrentBalance} />
