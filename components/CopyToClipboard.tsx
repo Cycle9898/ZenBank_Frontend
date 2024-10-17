d0ed75eabf4c968917aed911fcbea121d266b458
Cycle9898 <cycle9898@gmail.com>
2024-10-17 18:14:55 +0200

    fix some SVG attributes typos

--- components/CopyToClipboard.tsx
+++ components/CopyToClipboard.tsx
@@ -32,9 +32,9 @@ function CopyToClipboard({ title }: { title: string }) {
 					viewBox="0 0 24 24"
 					fill="none"
 					stroke="currentColor"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
+					strokeWidth="2"
+					strokeLinecap="round"
+					strokeLinejoin="round"
 					className="mr-2 size-4">
 					<rect width="14" height="14" x="8" y="8" rx="2" ry="2"></rect>
 					<path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"></path>
@@ -47,9 +47,9 @@ function CopyToClipboard({ title }: { title: string }) {
 					viewBox="0 0 24 24"
 					fill="none"
 					stroke="currentColor"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
+					strokeWidth="2"
+					strokeLinecap="round"
+					strokeLinejoin="round"
 					className="mr-2 size-4">
 					<polyline points="20 6 9 17 4 12"></polyline>
 				</svg>
